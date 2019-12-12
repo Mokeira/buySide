de149577fceed060dff0f3f509c99f2cdf464224
Angelah <angelahogutu@gmail.com>
2019-12-11 21:53:32 -0500

    Implemented functions

--- buysideAnswers.py
+++ buysideAnswers.py
@@ -1,28 +1,44 @@
 def countUppercase(file):
-    """
-    Given a file, return the total number of characters that are upper case
-    :param file: path to the file
-    :return: the number of characters that are upper case
-    """
-    pass
+    input_file = open(file,'r')
+    upper_case_count = 0
+
+    for line in input_file:
+        for char in line:
+            if char.isupper():
+                upper_case_count+=1
+
+    input_file.close()
+    return upper_case_count
 
 
-def fibonacci(n):
-    """
-    Calculate the nth Fibonacci number
-    :param n:
-    :return:
-    """
-    pass
+def fibonacci(n):    
+    a,b = 1,1
+
+    if n<=2:
+        return 1
+
+    for i in range(3,n+1):
+        c = a+b
+        a = b
+        b = c
+
+    return b
 
 
 def extract_ids(data):
-    pass
+    extracted_ids = [item["id"] for item in data]
+    return sorted(extracted_ids)
 
 
 def transformdata2(data):
-    pass
+    extracted_ids = extract_ids(data)
+    extracted_names = sorted([item["name"] for item in data],reverse=True)
+    return [(extracted_ids[i],extracted_names[i]) for i in range(len(extracted_ids))]
 
 
 def mergeDicts(a, b):
-    pass
+    for key,val in b.items():
+        if key not in a:
+            a[key]=val
+    
+    return a
