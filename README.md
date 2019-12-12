b0798c8c4de8ca58cb5d5b884efc7672133d83a6
Angelah <angelahogutu@gmail.com>
2019-12-11 21:49:50 -0500

    Initial Files

---
+++ README.md
@@ -0,0 +1,5 @@
+Please fill out buysideAnswers.py so that all the tests in buyside_test.py pass.
+Submit your entire project zipped via email or provide a link to a Github repo to joe@getbuyside.com.  
+Please provide instructions on how to run in a README.md, with any additional comments or feedback.
+
+Thanks!
---
+++ buysideAnswers.py
@@ -0,0 +1,28 @@
+def countUppercase(file):
+    """
+    Given a file, return the total number of characters that are upper case
+    :param file: path to the file
+    :return: the number of characters that are upper case
+    """
+    pass
+
+
+def fibonacci(n):
+    """
+    Calculate the nth Fibonacci number
+    :param n:
+    :return:
+    """
+    pass
+
+
+def extract_ids(data):
+    pass
+
+
+def transformdata2(data):
+    pass
+
+
+def mergeDicts(a, b):
+    pass
